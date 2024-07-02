1426aa75ee84e9116266aa997855685430e90d94
Piotr Brozek <1brozek.peter@gmail.com>
2024-07-02 12:45:17 -0500

    created layout

---
+++ movie-website/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    
+      <h1>title</h1>
+      <h2>subtitle</h2>
+
+    </>
+  );
+}
+
+export default App;
