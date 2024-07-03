3cb401e397d41343454ab9c970486327b0ed7e02
Piotr Brozek <1brozek.peter@gmail.com>
2024-07-03 12:59:35 -0500

    allowing top-movies button to be toggled on/off

--- client/src/NavBar.tsx
+++ client/src/NavBar.tsx
@@ -1,43 +1,44 @@
-
-
-const NavBar = ({data, setData}) => {
-    
-    const fetchMovies = () =>{
-
+const NavBar = ({ data, setData }) => {
+  const fetchMovies = () => {
     console.log("Getting top movies");
-    
+
     const apiServer = "http://localhost:3500";
-    fetch(`${apiServer}/get-top-movies`).then((res) => res.json()).then((data) => {
-      console.log("Value", data);
+    fetch(`${apiServer}/get-top-movies`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Value", data);
 
-      setData(data.results);
+        setData(data.results);
+      });
+  };
 
-    });
-    
-        
-    }
-    
-    return (
-      
-        <nav className="nav">
-        <a href="/" className="site-title">Best Movie Website</a>
-        <ul>
-            <li>
-                <button className="button" onClick={()=>fetchMovies()}>Top-Movies</button>
-             </li> 
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Best Movie Website
+      </a>
+      <ul>
+        <li>
+          <button
+            className="button"
+            onClick={() => {
+              if (data.length === 0) {
+                fetchMovies();
+              } else {
+                setData([]);
+              }
+            }}
+          >
+            Top-Movies
+          </button>
+        </li>
 
-
-             <li>
-
-                <button className="button">Search</button>
-
-            </li>
-
-        </ul>
-
+        <li>
+          <button className="button">Search</button>
+        </li>
+      </ul>
     </nav>
+  );
+};
 
-    )
-}
-
-export default NavBar
+export default NavBar;
