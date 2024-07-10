53446799324ab441f5572c707ae5e7fcf8259379
Piotr Brozek <1brozek.peter@gmail.com>
2024-07-10 12:26:08 -0500

    added modal when movie is clicked + need to attach summary to it

--- client/src/CardContainer.tsx
+++ client/src/CardContainer.tsx
@@ -1,5 +1,23 @@
 
+//For modal
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button
+} from '@chakra-ui/react'
+
+
+
+
 const CardContainer = ({data}) => {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const sayHello = (movieOver) =>
   {
@@ -15,10 +33,28 @@ const CardContainer = ({data}) => {
         .filter(movie => movie.backdrop_path) // Filter out movies with null backdrop_path
         .map((movie) => (
           <li key={movie.id} className="movie-item">
-            <img className="movie-image" src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={movie.title} onClick={() => sayHello(movie.overview)}/>
+            <img className="movie-image" src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={movie.title} onClick={onOpen}/>
             <div className="movie-title">{movie.title}</div>
           </li>
         ))}
+
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
     </ul>
   );
 }
