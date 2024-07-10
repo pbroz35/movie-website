eb8cc17456b98619754d4c6b8372a40a322b3cc5
Piotr Brozek <1brozek.peter@gmail.com>
2024-07-10 12:13:48 -0500

    reprogrammed new searchbar

--- client/src/NavBar.tsx
+++ client/src/NavBar.tsx
@@ -1,22 +1,7 @@
 import { useState } from "react";
-import {  ButtonGroup } from "@chakra-ui/react";
-import {
-  Button,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightAddon,
-  Box,
-  Heading,
-  Flex,
-  Spacer,
-  Stack,
-} from "@chakra-ui/react";
 
-import { Image } from "@chakra-ui/react";
-import { SearchBar } from "./components/SearchBar";
-
-
+import { Button, Box, Flex, Stack, Image } from "@chakra-ui/react";
+import { SearchBar } from "./SearchBar";
 
 const NavBar = ({ data, setData, search, setSearch }) => {
   const fetchMovies = () => {
@@ -33,64 +18,58 @@ const NavBar = ({ data, setData, search, setSearch }) => {
       });
   };
 
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-    console.log("set search");
-  };
-
-  const fetchSearch = () => {
-    console.log("Getting results for:", search);
-
-    const apiServer = "http://localhost:3500";
-
-    fetch(`${apiServer}/get-search?query=${search}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Value", data);
-
-        setData(data.results);
-
-        console.log("Fetched search from server.");
-      })
-      .catch((error) => {
-        console.error("Error fetching data", error);
-      });
-  };
+  
 
   return (
+    <Box
+      pt={3}
+      pl={6}
+      pr={6}
+      maxW="bg"
+      borderWidth="1.5px"
+      borderRadius="xl"
+      overflow="hidden"
+    >
+      <Flex align={"center-align"} justifyContent={"space-between"}>
+        <Box>
+          <Image
+            padding={1}
+            mt={-2}
+            ml={-1}
+            mb={1.4}
+            src="/new-icon.jpg"
+            alt="icon"
+            boxSize="100px" // Optional: Set the size of the image container
+            objectFit="cover"
+          />
+        </Box>
 
+        <Box width={525}>
+          <Stack spacing={3} direction={"row"} align={"center"} m={"0.5rem"}>
+            <Button
+              colorScheme="gray"
+              variant="solid"
+              onClick={() => {
+                if (data.length === 0) {
+                  fetchMovies();
+                } else {
+                  setData([]);
+                }
+              }}
+            >
+              Popular
+            </Button>
 
-    <Box pt={3} pl={6} pr={6} maxW='bg' borderWidth='1.5px' borderRadius='xl' overflow='hidden' >
-    <Flex align={"center-align"} justifyContent={"space-between"}>
-      <Box>
-        <Image padding={1} mt={-2} ml={-1} mb={1.4}
-          src="/new-icon.jpg"
-          alt="icon"
-          boxSize="100px" // Optional: Set the size of the image container
-          objectFit="cover"
-        />
-      </Box>
+            <Button colorScheme="gray" variant="solid">
+              Trending
+            </Button>
 
-      <Box width={525}>
-        <Stack spacing={3} direction={"row"} align={"center"} m={"0.5rem"}>
-
-          <Button colorScheme="gray" variant='outline' onClick={() => {
-              if (data.length === 0) {
-                fetchMovies();
-              } else {
-                setData([]);
-              }
-            }}>Popular</Button>
-
-          <Button colorScheme="gray" variant='outline'>Trending</Button>
-
-          <SearchBar></SearchBar>
-          
-        </Stack>
-      </Box>
-    </Flex>
-  </Box>
-
-  )}
+            <SearchBar data={data} setData={setData} search={search} setSearch={setSearch}></SearchBar>
+          </Stack>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
 
 export default NavBar;
---
+++ client/src/SearchBar.tsx
@@ -0,0 +1,67 @@
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightAddon,
+} from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+
+export const SearchBar = ({ data, setData, search, setSearch }) => {
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log("set search");
+  };
+
+  const fetchSearch = () => {
+    console.log("Getting results for:", search);
+
+    const apiServer = "http://localhost:3500";
+
+    fetch(`${apiServer}/get-search?query=${search}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Value", data);
+
+        setData(data.results);
+
+        console.log("Fetched search from server.");
+      })
+      .catch((error) => {
+        console.error("Error fetching data", error);
+      });
+  };
+
+  return (
+    <>
+      <InputGroup borderRadius={9} size="sm">
+        <InputLeftElement
+          pointerEvents="none"
+          children={<Search2Icon color="gray.600" />}
+        />
+        <Input
+          type="text"
+          placeholder="Find a movie..."
+          border="1px solid #949494"
+          onChange={(e) => updateSearch(e)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              fetchSearch();
+            }
+          }}
+        />
+        <InputRightAddon p={0} border="none">
+          <Button
+            size="sm"
+            borderLeftRadius={0}
+            borderRightRadius={3.3}
+            border="1px solid #949494"
+            onClick={() => fetchSearch()}
+          >
+            Search
+          </Button>
+        </InputRightAddon>
+      </InputGroup>
+    </>
+  );
+};
